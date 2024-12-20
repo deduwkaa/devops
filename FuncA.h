d74d183ab755af95a6beb91de608236d86d7bad0
Данил Римар <danylrymar@MacBook-Air--danik.local>
2024-12-20 02:03:29 +0200

    Hotfix: calculate sum of first 3 elements

--- FuncA.h
+++ FuncA.h
@@ -1,11 +1,18 @@
 #ifndef FUNCA_H
 #define FUNCA_H
 
+#include <cmath>
+
 class FuncA {
 public:
-    // Початкова реалізація: тригонометрична функція, яка повертає 0
+    // Реалізація для 3 елементів
     double calculate() {
-        return 0;
+        double result = 0;
+        double x = 1; // Встановіть x для прикладу
+        for (int i = 1; i <= 3; ++i) {
+            result += pow(-1, i - 1) * pow(x, i) / i;
+        }
+        return result;
     }
 };
 
